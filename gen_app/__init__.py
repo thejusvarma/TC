123bd531364fdb03b41081963254591408fcccb3
Thejus Varma <thejusvarma11@gmail.com>
2021-07-29 22:29:24 +0530

    TC generation

--- gen_app/__init__.py
+++ gen_app/__init__.py
@@ -1,9 +1,11 @@
-# this file is executed first and its set using command -> $env:FLASK_APP = "run.py"
-# change app to development server -> $env:FLASK_ENV = "development" 
-# activate debugger -> set FLASK_DEBUG= 1 
-# command to run the app -> python -m flask run
 from flask import Flask,escape,render_template,url_for,flash,redirect, request, abort
 from gen_app.forms import RegNumForm
+from PIL import Image,ImageFont,ImageDraw
+import pandas as pd
+import os
+import sys
+from pathlib import Path
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '48c92cc12c8608d0ae64d2615acf501a'
@@ -13,12 +15,15 @@ app.config['SECRET_KEY'] = '48c92cc12c8608d0ae64d2615acf501a'
 def home():
         form = RegNumForm()
         if form.validate_on_submit():
-            flash(f'{form.reg_num.data}','success')
-            return redirect(url_for('info'))
+            
+            # df = form.reg_num.data
+            # print(df, file=sys.stderr)
+            df = ["thejus", "shivarmpally", "btech"]
+            img  =  Image.open('gen_app\static\sample.jpg')
+            draw = ImageDraw.Draw(img)
+           
+            draw.text(xy=(200,200),text= '{}'.format(df[0]),fill=(0,0,0))
+            img.save(r'Downloads\{}.jpg'.format(df[0]))
+
+            return render_template('info.html',title='Student Info' )
         return render_template('home.html',title='home',form=form)
-
-
-@app.route('/info',methods=['GET','POST'])
-def info():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'

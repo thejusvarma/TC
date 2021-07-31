54b10f6fbff678bdfc941d17691702ab6811596f
Thejus Varma <thejusvarma11@gmail.com>
2021-07-31 16:38:28 +0530

    excel upload added

--- gen_app/__init__.py
+++ gen_app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_file,escape,render_template,url_for,flash,redirect, request, abort,send_file, send_from_directory, safe_join
-from gen_app.forms import RegNumForm
+from gen_app.forms import rollnumform, uploadfile
 from PIL import Image,ImageFont,ImageDraw
 import pandas as pd
 import os
@@ -11,19 +11,45 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '48c92cc12c8608d0ae64d2615acf501a'
 
-
 @app.route('/home',methods=['GET','POST'])
 def home():
-        form = RegNumForm()
-        if form.validate_on_submit():
-            df = ["thejus", "shivarmpally", "btech"]
+        form = rollnumform()
+        form2 = uploadfile()
+
+       
+        if form.roll_num.data:
+            # input_roll_num = form.roll_num.data
+            # excel_data = pd.read_excel(form2.uploaded_file)
+            # df[df["Country"] == '{{}}'].head()
+            df = ['thej','dffd']
             img  =  Image.open('gen_app\static\TC.jpg')
             draw = ImageDraw.Draw(img)
-            draw.text(xy=(200,200),text= '{}'.format(df[0]),fill=(0,0,0))
+            draw.text(xy=(200,200),text='{}'.format(df[0]),fill=(0,0,0))
             img.save(r'gen_app\static\saved\{}.pdf'.format(df[0]))
-            
             return render_template('info.html',title='Student Info',name=df[0])
-        return render_template('home.html',title='home',form=form)
+
+        if form2.uploaded_file.data:
+            # data = pd.read_excel(form2.uploaded_file)
+            filename = secure_filename(form2.uploaded_file.data.filename)
+            filepath = os.path.join(r'gen_app\static','excel')
+            form2.uploaded_file.data.save(filepath)
+            return render_template('info.html',title='home',form=form,form2=form2)
+
+
+
+        return render_template('home.html',title='home',form2=form2,form=form)
+
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload():
+    
+#         if form2.validate_on_submit():
+#             # data = pd.read_excel(form2.uploaded_file)
+#             filename = secure_filename(form2.uploaded_file.data)
+#             filepath = os.path.join(r'gen_app\static',filename)
+#             form2.uploaded_file.data.save(filepath)
+#             print('This', file=sys.stderr)
+#             return render_template('home.html',title='home',form=form,form2=form2)
 
 @app.route('/return-file/', methods=['GET', 'POST'])
 def file_download():
--- gen_app/forms.py
+++ gen_app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField, TextAreaField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 
-class RegNumForm(FlaskForm):
-    reg_num = StringField('Registration Number', validators=[DataRequired()])
+class rollnumform(FlaskForm):
+    roll_num = StringField('Roll Number', validators=[DataRequired()])
     submit = SubmitField('Check')
     # def validate_username(self,username):
     #     user = User.query.filter_by(username = username.data).first()
@@ -14,3 +15,6 @@ class RegNumForm(FlaskForm):
     #     user = User.query.filter_by(email = email.data).first()
     #     if user:
     #         raise ValidationError('E-Mail Id already exists')
+class uploadfile(FlaskForm):
+    uploaded_file = FileField('Upload File',validators=[DataRequired(),FileAllowed(['xlsx'])])
+    submit = SubmitField('Submit')
